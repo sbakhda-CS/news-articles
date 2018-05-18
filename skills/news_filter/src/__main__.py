fbe6fd3b7649d6814e1ac5797e04de2778178136
Shikhar Bakhda <sbakhda@cognitivescale.edu>
2018-05-18 18:17:26 -0500

    created error conditions and handled edge cases for all the filters

--- skills/news_filter/src/__main__.py
+++ skills/news_filter/src/__main__.py
@@ -1,5 +1,5 @@
 from cortex_client import InputMessage, OutputMessage
-import requests, json
+import requests
 
 
 # the headline function returns the top n headlines for any news source
@@ -49,16 +49,27 @@ def filter_news(country, category, source, query, batch_size, batch_no, api_toke
         if category and not source:
             url += '&category=' + category
         if query:
-            url += '&query=' + query
+            url += '&q=' + query
         if batch_size:
             url += '&pageSize=' + str(batch_size)
         if batch_no:
             url += '&page=' + str(batch_no)
 
+        if not(source or query or country or category):
+            return None, None, "noParams", "Add more parameters, such as country, category, query or  source"
         response = requests.get(url).json()
-        error_check(response)
+        e = error_check(response)
+        if e is not None: return e
+
 
     elif filter_type == 'source_filter':
+
+        if query is not None or query != "":
+            return None, None, "wrongParams", "Query parameter is not applicable for source filter"
+
+        if not(source or country or category):
+            return None, None, "noParams", "Add more parameters, such as source, country or category"
+
         url += 'sources?apiKey=' + api_token
         if country:
             url += '&country=' + country
@@ -86,19 +97,30 @@ def filter_news(country, category, source, query, batch_size, batch_no, api_toke
 
         response['sources'] = ss[beg: end]
 
+        e = error_check(response)
+        if e is not None: return e
+
     elif filter_type == 'all_filter':
+
+        if country or category:
+            return None, None, "wrongParams", "Country and category parameters are not applicable for all filter"
+
+        if not(source or query):
+            return None, None, "noParams", "Add more parameters, such as query and source"
+
         url += 'everything?apiKey=' + api_token
         if source:
-            url += '&source=' + source
+            url += '&source=' + str(source)
         if query:
-            url += '&query=' + query
+            url += '&q=' + str(query)
         if batch_size:
             url += '&pageSize=' + str(batch_size)
         if batch_no:
             url += '&page=' + str(batch_no)
 
         response = requests.get(url).json()
-        error_check(response)
+        e = error_check(response)
+        if e is not None: return e
 
     else:
         status = None
@@ -112,8 +134,24 @@ def filter_news(country, category, source, query, batch_size, batch_no, api_toke
     elif response.get('sources'):
         return response.get('status'), response.get('sources'), None, None
 
+    return response.get('status'), [], response.get('code'), response.get('message')
 
 def error_check(response):
+    if response is None:
+        return None, None, "noResponse", "No response was recieved"
+
     if response.get('status') == 'error' or \
             not(response.get('code') is None or response.get('message') is  None):
         return None, None, response.get('code'), response.get('message')
+
+    if (response.get('articles') is None and len(response.get('sources')) == 0) \
+        or (response.get('sources') is None and len(response.get('articles')) == 0) :
+        return None, None, "noResults", "No results are available for your request"
+
+    return None
+
+
+# print(filter_news(None, None, None, "ben+ka+loda",1,1, "c474c16b92274c538f647d92387b00c7","all_filter"))
+# print(filter_news(None, None, None, "ben+ka+loda",1,1, "c474c16b92274c538f647d92387b00c7","source_filter"))
+# print(filter_news("in", "technology", "bbc-news" , "IT",10,1, "c474c16b92274c538f647d92387b00c7","top_filter"))
+
