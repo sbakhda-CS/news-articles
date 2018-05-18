73da2f2c20d8b88b678280bd645e46bd4271cf39
Shikhar Bakhda <sbakhda@cognitivescale.edu>
2018-05-18 14:51:35 -0500

    implemented batch size/no for source filter

--- skills/news_filter/src/__main__.py
+++ skills/news_filter/src/__main__.py
@@ -21,7 +21,7 @@ def main(params):
     filter_type = msg.properties.get('filter_type')
 
     status, results, error_code, error_message = \
-        filter(country, category, source, query, batch_size, batch_no, api_token, filter_type)
+        filter_news(country, category, source, query, batch_size, batch_no, api_token, filter_type)
 
     if not error_code:
         return OutputMessage.create().with_payload(
@@ -36,14 +36,14 @@ def main(params):
 # source: news source from news api
 # n: number of top articles from source
 # api: api token for the website newsapi.org
-def filter(country, category, source, query, batch_size, batch_no, api_token, filter_type):
+def filter_news(country, category, source, query, batch_size, batch_no, api_token, filter_type):
 
     url = 'https://newsapi.org/v2/'
 
     if filter_type == 'top_filter':
         url += 'top-headlines?apiKey=' + api_token
         if source:
-            url += '&source=' + source
+            url += '&sources=' + source
         if country and not source:
             url += '&country=' + country
         if category and not source:
@@ -55,14 +55,36 @@ def filter(country, category, source, query, batch_size, batch_no, api_token, fi
         if batch_no:
             url += '&page=' + str(batch_no)
 
+        response = requests.get(url).json()
+        error_check(response)
+
     elif filter_type == 'source_filter':
         url += 'sources?apiKey=' + api_token
         if country:
             url += '&country=' + country
         if category:
             url += '&category=' + category
+
+        response = requests.get(url).json()
+        error_check(response)
+
+        ss = response.get('sources')
+
         if source:
-            pass
+            ss = list(filter(lambda x: x.get('id') == source, ss))
+
+        if batch_size < 1 or batch_no < 1:
+            beg = 0
+        else:
+            beg = batch_size * (batch_no - 1)
+            if beg > len(ss): beg = 0
+        if batch_size > len(ss) or batch_no > len(ss):
+            end = len(ss)
+        else:
+            end = batch_size * batch_no
+            if end > len(ss): end = len(ss)
+
+        response['sources'] = ss[beg: end]
 
     elif filter_type == 'all_filter':
         url += 'everything?apiKey=' + api_token
@@ -75,6 +97,9 @@ def filter(country, category, source, query, batch_size, batch_no, api_token, fi
         if batch_no:
             url += '&page=' + str(batch_no)
 
+        response = requests.get(url).json()
+        error_check(response)
+
     else:
         status = None
         results = None
@@ -82,12 +107,13 @@ def filter(country, category, source, query, batch_size, batch_no, api_token, fi
         error_message = 'wrong filter'
         return status, results, error_code, error_message
 
-    response = requests.get(url).json()
+    if response.get('articles'):
+        return response.get('status'), response.get('articles'), None, None
+    elif response.get('sources'):
+        return response.get('status'), response.get('sources'), None, None
 
-    if response['status'] != 'error' and not (response['code'] or response['message']):
-        if response['articles']:
-            return response['status'], response['articles'], None, None
-        elif response['sources']:
-            return response['status'], response['sources'], None, None
-    else:
-        return None, None, response['code'], response['message']
+
+def error_check(response):
+    if response.get('status') == 'error' or \
+            not(response.get('code') is None or response.get('message') is  None):
+        return None, None, response.get('code'), response.get('message')
